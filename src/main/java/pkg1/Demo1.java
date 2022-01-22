7f0a251da35e09c36e7c5273068882d2b2c13937
Avishek <avishekbehera87@gmail.com>
2022-01-22 09:42:59 +0530

    Initial Test commit for framework

---
+++ src/main/java/pkg1/Demo1.java
@@ -0,0 +1,9 @@
+package pkg1;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("My First Java Program");
+    }
+
+}
---
+++ src/test/java/testpkg1/TestC.java
@@ -0,0 +1,20 @@
+package testpkg1;
+
+import org.testng.annotations.Test;
+
+public class TestC {
+
+    @Test
+    public void myTestMethod1() {
+        System.out.println(" Test method1");
+    }
+    @Test
+    public void myTestMethod2() {
+        System.out.println(" Test method2");
+    }
+    @Test
+    public void myTestMethod3() {
+        System.out.println(" Test method3");
+    }
+
+}
