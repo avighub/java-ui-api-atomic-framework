948c80430cb94958a2478ae4b1f3070c7a2eeb22
SimmiSinha <simmisnh@gmail.com>
2022-01-27 20:24:08 -0700

    Added a new testng file

--- src/test/java/testpkg1/Testweb.java
+++ src/test/java/testpkg1/Testweb.java
@@ -6,16 +6,20 @@ public class Testweb {
     @Test
     public void googletest()
     {
+
         System.out.println("User is on Google Home Page http:google.com");
     }
     @Test
-    public void googlesearch()  {
-        System.out.println("User enters keyword to serach 'automation testing'");
+    public void googlesearch() throws InterruptedException
+    {
+        System.out.println("User enters keyword to search 'automation testing'");
+        Thread.sleep(2000);
 
     }
     @Test
     public void googleresult()
     {
+
         System.out.println("User gets the results in the google page");
     }
 }
