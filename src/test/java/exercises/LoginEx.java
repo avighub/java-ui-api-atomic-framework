c520652905d2ba0e57c4c4d1080f4ab2861829e3
SimmiSinha <simmisnh@gmail.com>
2022-02-01 09:31:51 -0700

    Added a new testng file

---
+++ src/test/java/exercises/LoginEx.java
@@ -0,0 +1,45 @@
+package exercises;
+
+import org.testng.annotations.Test;
+
+public class LoginEx {
+    @Test(priority = 0)
+    public void loginweb(){
+        String URL = "https://www.saucedemo.com/";
+        String Username = "standard_user";
+        String Pwd = "secret_sauce";
+        System.out.println("User Will Navigate to " + URL);
+
+        System.out.println("User will pass username in the field and sendkeys = " + Username);
+
+        System.out.println("User will pass password in the PWD field and sendkeys = "+ Pwd);
+    }
+    @Test(priority = 1)
+    public void addcart(){
+        String URLC = "user is on this link https://www.saucedemo.com/inventory.html";
+
+        System.out.println(URLC);
+
+        System.out.println("User will find the element 'Sauce Labs Fleece Jacket' after logging in.");
+        System.out.println("User will find the element 'ADD TO CART' and click it!");
+        System.out.println("User will verify the button count is changed to '1' ");
+    }
+    @Test(priority = 2)
+    public void productinfo(){
+        String URLCart = "https://www.saucedemo.com/cart.html";
+        System.out.println("User will go to 'Your Cart' page " + URLCart);
+        System.out.println("verify the product name, description and price");
+    }
+    @Test(priority = 3)
+    public void productverify(){
+        String URLinv = "https://www.saucedemo.com/inventory.html";
+        System.out.println("The user is logged in and is on the products page "+ URLinv);
+        System.out.println("Find element 'Sauce Labs Backpack' and click");
+        System.out.println("Validate user is on the product info page of the same product");
+        System.out.println("validate product image, title, description, price is present");
+
+    }
+
+
+}
+
