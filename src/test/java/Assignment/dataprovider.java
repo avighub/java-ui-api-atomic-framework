7a7c291b961f71be570de7ee80805d9857c1d6c9
pradee <pradeesuresh19@gmail.com>
2022-02-02 18:56:56 -0800

    Assigment1 added priority and enabled

---
+++ src/test/java/Assignment/dataprovider.java
@@ -0,0 +1,36 @@
+package Assignment;
+
+import org.testng.annotations.Test;
+
+public class dataprovider()
+{
+
+    @Test
+    public void login()
+    {
+
+        String URL= ("https://www.saucedemo.com/");
+        String username = ("standard_user");
+        String password = ("secret-sauce");
+        System.out.println("Application launched successfully");
+    }
+    @Test(priority=1,enabled=false)
+    public void AddCart()
+    {
+
+        String URL= ("https://www.saucedemo.com/inventory.html");
+        System.out.println("user lands on product page");
+        System.out.println("User clicks add to cart on sauce lab fleece jacket");
+        System.out.println("user can see quantity 1 in cart");
+    }
+
+    @Test
+    public void ProductCartPage()
+    {
+        String URL= ("https://www.saucedemo.com/cart.html");
+        System.out.println("user lands on product page");
+        System.out.println("User clicks on product sauce lab backpack");
+
+        System.out.println("user validates all the description and price matches");
+    }}
+
