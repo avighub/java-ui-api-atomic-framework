338754e5a2d2d83a3cd0ac3c7aab4e134a84cae5
Avishek <avishekbehera87@gmail.com>
2022-02-03 09:35:50 +0530

    Review Code-Part1

--- src/test/java/Assignment/SimmiLoginEx.java
+++ src/test/java/Assignment/SimmiLoginEx.java
@@ -1,9 +1,9 @@
-package exercises;
+package Assignment;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class LoginEx {
+public class SimmiLoginEx {
 
     @DataProvider(name = "login-data")
     public Object[][] loginData() {
@@ -11,19 +11,20 @@ public class LoginEx {
         //Return Object
         return new Object[][]{{"standard_user", "secret_sauce"}, {"locked_out_user", "secret_sauce"}};
     }
+
     @Test(priority = 0, dataProvider = "login-data")
-    public void loginweb(String username, String password)
-    {
+    public void loginweb(String username, String password) {
         String URL = "https://www.saucedemo.com/";
 
         System.out.println("User Will Navigate to " + URL);
 
         System.out.println("User will pass username in the field and sendkeys = " + username);
 
-        System.out.println("User will pass password in the PWD field and sendkeys = "+ password);
+        System.out.println("User will pass password in the PWD field and sendkeys = " + password);
     }
+
     @Test(priority = 1)
-    public void addcart(){
+    public void addcart() {
         String URLC = "user is on this link https://www.saucedemo.com/inventory.html";
 
         System.out.println(URLC);
@@ -32,16 +33,18 @@ public class LoginEx {
         System.out.println("User will find the element 'ADD TO CART' and click it!");
         System.out.println("User will verify the button count is changed to '1' ");
     }
+
     @Test(priority = 2)
-    public void productinfo(){
+    public void productinfo() {
         String URLCart = "https://www.saucedemo.com/cart.html";
         System.out.println("User will go to 'Your Cart' page " + URLCart);
         System.out.println("verify the product name, description and price");
     }
+
     @Test(priority = 3)
-    public void productverify(){
+    public void productverify() {
         String URLinv = "https://www.saucedemo.com/inventory.html";
-        System.out.println("The user is logged in and is on the products page "+ URLinv);
+        System.out.println("The user is logged in and is on the products page " + URLinv);
         System.out.println("Find element 'Sauce Labs Backpack' and click");
         System.out.println("Validate user is on the product info page of the same product");
         System.out.println("validate product image, title, description, price is present");
--- src/test/java/Assignment/dataprovider.java
+++ src/test/java/Assignment/dataprovider.java
@@ -2,35 +2,33 @@ package Assignment;
 
 import org.testng.annotations.Test;
 
-public class dataprovider()
-{
+public class dataprovider {
 
     @Test
-    public void login()
-    {
+    public void login() {
 
-        String URL= ("https://www.saucedemo.com/");
+        String URL = ("https://www.saucedemo.com/");
         String username = ("standard_user");
         String password = ("secret-sauce");
         System.out.println("Application launched successfully");
     }
-    @Test(priority=1,enabled=false)
-    public void AddCart()
-    {
 
-        String URL= ("https://www.saucedemo.com/inventory.html");
+    @Test(priority = 1, enabled = false)
+    public void AddCart() {
+
+        String URL = ("https://www.saucedemo.com/inventory.html");
         System.out.println("user lands on product page");
         System.out.println("User clicks add to cart on sauce lab fleece jacket");
         System.out.println("user can see quantity 1 in cart");
     }
 
-    @Test(parallel=true)
-    public void ProductCartPage()
-    {
-        String URL= ("https://www.saucedemo.com/cart.html");
+    //    @Test(parallel=true)
+    public void ProductCartPage() {
+        String URL = ("https://www.saucedemo.com/cart.html");
         System.out.println("user lands on product page");
         System.out.println("User clicks on product sauce lab backpack");
 
         System.out.println("user validates all the description and price matches");
-    }}
+    }
+}
 
