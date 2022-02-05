979e5e8a8f558c8eb0d08bca1507fa8dbe0ae939
manjulasurakanti <manjulasurakanti77@gmail.com>
2022-02-04 18:35:00 -0600

    created a sample test class and added 2 test cases

---
+++ src/test/java/testng/SampleTestClass.java
@@ -0,0 +1,17 @@
+package testng;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class SampleTestClass {
+
+    @Test
+    public void testSomething(){
+        System.out.println("test something");
+
+    }
+    @Test
+    public void testOtherThing(){
+        System.out.println("test other thing");
+    }
+}
